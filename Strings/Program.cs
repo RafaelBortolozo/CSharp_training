88f5f72bb28a59343f962c99b3a67ad94dfe0b33
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-07-27 18:00:42 -0300

    (27/07)

---
+++ Strings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GUID
+
+            // var id = Guid.NewGuid(); //id unico
+            // id.ToString();
+            // Console.WriteLine(id);
+            // id = new Guid("8688615a-d378-4847-9c73-a0b0333ed53d"); //converte string pra Grid
+            // Console.WriteLine(id);
+
+            /***************************************************************/
+            // INTERPOLAÇÃO DE STRINGS
+
+            //var price = 10.2;
+            //var texto = "O preço do produto é " + price + " apenas na promoção";
+            //var texto = string.Format("O preço do produto é {0} apenas na {1}", price, "promoção");
+            //var texto = $"O preço do produto é {price} apenas na promoção";
+            //var texto = $@"O preço do 
+            //                produto é {price} 
+            //                apenas na promoção";
+            //Console.WriteLine(texto);
+
+            /***************************************************************/
+            // COMPARAÇÃO DE STRINGS
+        }
+    }
+}
