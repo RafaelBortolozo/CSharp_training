a6dee7f575838b35248fa33fa5e79c0242b11842
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2022-07-28 17:27:23 -0300

    (28/07)

---
+++ Coins/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            decimal valor = 10536.25m; //alta precisão
+            Console.WriteLine(valor.ToString("C" ,CultureInfo.CreateSpecificCulture("pt-BR"))); // imprime em reais
+            
+            Console.WriteLine(Math.Round(valor)); //arredonda
+            Console.WriteLine(Math.Ceiling(valor)); //arredonda pra cima
+            Console.WriteLine(Math.Floor(valor)); //arredonda pra baixo
+
+        }
+    }
+}
